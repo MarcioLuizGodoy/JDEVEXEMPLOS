0c7d227fe9ade2c4ba4b553ddfb7cc2fb9a10020
MarcioLuizGodoy <marciooliveirasilvagodoy@gmail.com>
2023-12-03 16:38:19 -0300

    criando atributos

--- Primeiro_Programa/src/Aluno.java
+++ Primeiro_Programa/src/Aluno.java
@@ -3,5 +3,11 @@ public class Aluno {
 String nome;
 int idade;
 String cpf;
+String dataNascimento;
+String nomeMae;
+String nomePai;
+String dataMatricula;
+String nomeEscola;
+String serieMatriculado;
 
 }
