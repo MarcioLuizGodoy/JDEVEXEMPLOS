2fc241b64a27a0064903926e98b59a51112e0b50
MarcioLuizGodoy <marciooliveirasilvagodoy@gmail.com>
2024-01-06 03:23:21 -0300

    criando classe Pessoa e praticando heranca

---
+++ Primeiro_Programa/src/Pessoa.java
@@ -0,0 +1,11 @@
+
+public class Pessoa {
+
+	private String nome;
+	private int idade;
+	private String cpf;
+	private String dataNascimento;
+	private String nomeMae;
+	private String nomePai;
+	private String registroGeral;
+}
